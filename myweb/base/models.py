5cc7d2752e2286d795493a54b2f62300b731b137
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-06-20 20:46:03 +0400

    model Book

--- myweb/base/models.py
+++ myweb/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    author = models.CharField(max_length=200)
+    picture = models.CharField(max_length=300)
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=500)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} _ {self.author}"
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book
 
 # Create your views here.
 def home(request):
-    return render(request, 'base/home.html')
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
     return render(request, 'base/about.html')
+
+
+def contact(request):
+    return render(request, 'base/contact.html')
